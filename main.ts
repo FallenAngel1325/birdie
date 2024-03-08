213b027f7e7ee6fc6ccff635c320d8bca23cfa07
The-Genius001 <jcr4576@gmail.com>
2024-03-08 12:32:00 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
+let compass = 0;
 finch.startFinch()
 finch.setMove(MoveDir.Forward, 10, 50)
 basic.forever(function () {
     basic.showIcon(IconNames.Yes)
 })
-hi
+function Circle(rad, dir) {
+    let compass = finch.getFinchCompass();
+    if (dir=1){
+        finch.startMotors(100, 100*rad);
+    }
+    else if (dir = 0) {
+        finch.startMotors(100*rad, 100);
+    }
+    
+        basic.pause(100)
+        loops.everyInterval(1, function() {
+        });
+    
+}
+function checkCompass(){
+    if (compass = finch.getFinchCompass()){
+        finch.stopMotors()
+    }
+}
